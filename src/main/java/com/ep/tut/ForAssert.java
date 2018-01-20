6327cd80f7c694daf37ac56168264d663f4a2d69
Dzmitry_Kupratsevich <Dzmitry_Kupratsevich@epam.com>
2018-01-20 17:40:58 +0300

    just commit

---
+++ src/main/java/com/ep/tut/ForAssert.java
@@ -0,0 +1,15 @@
+package com.ep.tut;
+
+public class ForAssert {
+
+    private int one = 1;
+
+    public int returnInt() {
+
+        int a = 0;
+        if (one == 1) {
+            a = a + 2;
+        } else a = 1;
+        return a;
+    }
+}
---
+++ src/test/java/com/ep/tut/myTest.java
@@ -0,0 +1,17 @@
+package com.ep.tut;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class myTest {
+
+    ForAssert forAssert = new ForAssert();
+
+    int d = forAssert.returnInt();
+
+    @Test
+    public void myT() {
+        Assert.assertTrue(4 != d);
+    }
+}
